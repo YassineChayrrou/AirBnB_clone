0438bd9a262622215323b708173ccef2b9a0c885
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-02-15 10:27:32 +0000

    pep8 conforment and empty line handled

--- models/console.py
+++ models/console.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    intro='''
-             __   __  _______  _______  _______  ___   _______ 
-            |  | |  ||   _   ||       ||       ||   | |       |
-            |  |_|  ||  |_|  ||  _____||____   ||   | |____   |
-            |       ||       || |_____  ____|  ||   |  ____|  |
-            |_     _||       ||_____  || ______||   | | ______|
-              |   |  |   _   | _____| || |_____ |   | | |_____ 
-              |___|  |__| |__||_______||_______||___| |_______|
-    ''' 
-    prompt='(hbnb) '
+    """HBNBCommand Class that inherits from cmd
+       acts as command line console"""
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        """emptyline handler"""
+        if self.lastcmd is None:
+            return self.onecmd('\n')
 
     def help(self):
         print(''.join([
             'Documented commands (type help <topic>):',
             '========================================',
                 ]))
- 
-    def emptyline():
-
 
     def do_quit(self, *args):
+        "Quit command to exit the program\n"
         return True
-
-    def help_quit(self, *args):
-        print("Quit command to exit the program\n")
-
 
     def do_EOF(self, *args):
+        "EOF: type Ctrl + d to exit"
         return True
-
-    def help_EOF(self, *args):
-        print("End Of File")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
