ce64c134d0f72dfc0ef7d6f43e27d3a6d299b5f1
SpacyFranky <bigaziz@outlook.com>
2020-02-18 10:59:56 +0000

    fixed adding the correct attributes

--- models/base_model.py
+++ models/base_model.py
@@ -26,9 +26,13 @@ class BaseModel:
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key != "__class__":
-                    self.key = value
-                if key == "created_at" or key == "updated_at":
-                    self.key = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    if key == "created_at":
+                        self.created_at = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                    if key == "updated_at":
+                        self.updated_at = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         # end of task 4
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
