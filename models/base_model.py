a86a6a26db61754b4bf3fc6d433ee276ca831ec6
SpacyFranky <bigaziz@outlook.com>
2020-02-18 10:07:50 +0000

    checking task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,51 @@
+#!/usr/bin/python3
 """
 BaseModel that defines all common attributes/methods for other classes.
+dev branch
 """
+from datetime import datetime
 import uuid
-import datetime
+
 
 class BaseModel:
     """BaseModel class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Class constructor, creates a new instance
            of a class
+           Args:
+               *args (tuple): arguments of function
+               **kwargs (dict): argument dictionary of function
            Public Attrib:
                id (str): id of an instance when it's created
                created_at (datetime):
                updated_at (datetime):
         """
+        # task 4
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    self.key = value
+                if key == "created_at" or key == "updated_at":
+                    self.key = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+        # end of task 4
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Prints this format:
                [<class name>] (<self.id>) <self.__dict__>
         """
-        return("[{}] ({}) <{}>".format(self.__class__.__name__,
-                                       self.id, self.__dict__))
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__))
 
     def save(self):
         """Updates the public instance attribute updated_at
            with the current datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values
