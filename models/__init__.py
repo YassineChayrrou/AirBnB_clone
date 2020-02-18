ca1a628a908f74650ab47c8f8ac82e33fd13ed2d
SpacyFranky <bigaziz@outlook.com>
2020-02-18 09:52:36 +0000

    still in dev

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-init file
-"""
-from models.engine import file_storage
-
-storage = file_storage.FileStorage()
-storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Storing first object
 """
@@ -9,30 +10,19 @@ class FileStorage:
     """A class that serializes instances to a JSON file and deserializes
        JSON file to instances
     """
-    __file_path = "storage.json"
-    __objects = {}
 
     def all(self):
         """returns the dictionary __objects
         """
-        return(FileStorage.__objects)
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id
         """
-        FileStorage.__objects[obj.__class__.__name__.id] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)
         """
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(obj)
-
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON
            file (__file_path) exists)
         """
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as f:
-                load_json = json.load(f)
-                return(load_json)
