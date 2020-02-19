017817374905652532dd5ffaf256a2e3b9d8abe0
SpacyFranky <bigaziz@outlook.com>
2020-02-19 13:25:10 +0000

    storing objects testing if it works

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,20 +21,26 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id
         """
-        s = obj.__class__.__name__ + "." + obj.id
+        s = str(obj.__class__.__name__) + "." + str(obj.id)
         FileStorage.__objects[s] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)
         """
+        d = {}
+        for key, value in FileStorage.__objects.items():
+            d[key] = value.to_dict()
         with open(FileStorage.__file_path, 'w') as f:
-            f.write(json.dumps(FileStorage.__objects))
+            json.dump(d, f)
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON
            file (__file_path) exists)
         """
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as f:
-                load_json = json.loads(f)
-                return(load_json)
+        try:
+            if os.path.isfile(FileStorage.__file_path):
+                with open(FileStorage.__file_path, 'r') as f:
+                    for key, value in FileStorage.__objects.items():
+                        FileStorage.__objects[key] = json.load(value)
+        except:
+            pass
