6baa5b57b77f2008b4c7c1acf0811a4200205485
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-02-19 19:03:43 +0000

    update function still miss one functionality

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ import json
 from models.base_model import BaseModel
 class_list = ('User', 'City', 'BaseModel')
 uuid_list = ('test', 'test2', '1234')
+attribute_list = ('name','email','something_else')
+
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand Class that inherits from cmd
@@ -16,27 +18,71 @@ class HBNBCommand(cmd.Cmd):
         if parsed:
             if parsed[0] in class_list:
                 model = BaseModel()
+                #save method by aziz
                 return print("{}".format(model.id))
             return print("** class doesn't exist **")
         print("** class name missing **")
 
     def do_show(self, arg):
-        """Prints the string representation of instance id/class
-Requires class_name and id"""
+        """Prints the string representation of an instance
+based on class name and id
+        """
         parsed = parse(arg)
         if len(parsed) >= 1:
             if parsed[0] in class_list:
                 model = BaseModel()
+                model.first_name = "Yassine"
+                model = BaseModel()
                 if len(parsed) >= 2:
                     if parsed[1] in uuid_list:
-                        # condition to check if id does exist is missing
-                        # checks if id is true code goes here
-                        # tobe done with Franky Spacy
+                        print("still under active development")
                         return print("checked:\n{}".format(model))
                     return print("** no instance found **")
-                return print("**instance id missing**")
+                return print("** instance id missing **")
             return print("** class doesn't exist **")
         return print("** class name missing **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        parsed = parse(arg)
+        if len(parsed) >= 1:
+            if parsed[0] in class_list:
+                if len(parsed) >= 2:
+                    if parsed[1] in uuid_list:
+                        return print("under active development")
+                    return print("** no instance found **")
+                return print("** instance id missing **")
+            return print("** class doesn't exist **")
+        return print("** class name missing **")
+
+    def do_all(self, arg):
+        parsed = parse(arg)
+        if len(parsed) == 0:
+            #prints storage.json in case of argument all(only)
+            return print("prints all classes string representation")
+        if parsed[0] in class_list:
+            model = BaseModel()
+            return print('mimics all string rep of class <BaseModel>\n["{}"]'.format(model))
+        return print("** class doesn't exit **")
+
+    def do_update(self, arg):
+       parsed = parse(arg)
+       if len(parsed) >= 1:
+           if parsed[0] in class_list:
+               if len(parsed) >= 2:
+                   if parsed[1] in uuid_list:
+                       if len(parsed) >= 3:
+                           if len(parsed) >= 4:
+                               if parsed[3][0] == '"':
+                                   quote = arg.split('"')
+                                   return print(quote[1])
+#                                   return print("<{}> is set and to be added here ------///".format(parsed[3]))
+                           return print("** value missing **")
+                       return print("** attribute name missing **")
+                   return print("** no instance found **")
+               return print("** instance id missing **")
+           return print("** class doesn't exist **")
+       return print("** class name missing **")
 
     def emptyline(self):
         """emptyline handler"""
